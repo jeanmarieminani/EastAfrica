5c8f879cd0ccb6f211997e12badb82d203faa9cd
Jean-Marie Minani <jeanmarie.minani2@gmail.com>
2024-12-05 15:15:59 +0000

    second update precipitation and temperature data in nasa_power_data_csv

--- nasa_power_data_csv/delete_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Replace 'region_1_TS_' with the desired pattern
-pattern="region_1_TS_"
-
-# Find and delete files matching the pattern
-find . -name "*$pattern*" -delete
--- nasa_power_data_csv/delete_multiple_patterns.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Define the patterns to match
-patterns=("region_2_TS_" "region_3_TS_" "region_4_TS_" "region_5_TS_" "region_6_TS_")
-
-# Iterate over each pattern and delete matching files
-for pattern in "${patterns[@]}"; do
-  find . -name "*$pattern*" -delete
-done
